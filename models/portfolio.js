2d2c8b44100bc3b771ec9124fe8b0e5cde02cc6c
Amanda Wang <amandawang@Amandas-MacBook-Pro.local>
2017-01-28 11:55:01 -0500

    added / route

--- models/portfolio.js
+++ models/portfolio.js
@@ -1,36 +1,34 @@
 module.exports = function(sequelize, DataTypes) {
-  var Portfolio = sequelize.define("Portfolio", {
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [1]
-      }
-    },
-    imageURL: {
-      type: DataTypes.TEXT,
-      validate:{
-          isURL:true
-    }
-  },
-    description: {
-      type: DataTypes.TEXT,
-      allowNull: false,
-      len: [1]
-    }
-  },
-    {   
-      classMethods: {
-        associate: function(models) {
-          Portfolio.belongsTo(models.User,
-            {
-              onDelete: "cascade",
-              foreignKey: {
-                allowNull: false
-              }
-            });
+    var Portfolio = sequelize.define("Portfolio", {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1]
+            }
+        },
+        imageURL: {
+            type: DataTypes.TEXT,
+            validate: {
+                isURL: true
+            }
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+            len: [1]
         }
-      }
+    }, {
+        classMethods: {
+            associate: function(models) {
+                Portfolio.belongsTo(models.User, {
+                    onDelete: "cascade",
+                    foreignKey: {
+                        allowNull: false
+                    }
+                });
+            }
+        }
     });
-  return Portfolio;
+    return Portfolio;
 };
